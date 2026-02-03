0a21c00c2d5435477dcb0e3c15f7ebdc04ea460a
jmasalunga2025-arch <jmasalunga2025@student.nbscollege.edu.ph>
2026-02-03 19:10:06 +0800

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+age += 1
+
+print(f"Hello, {name}. You will be {age} in one year.")
+print(f"Your name has {len(name)} characters.")
