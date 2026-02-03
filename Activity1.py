9fb1b4c4b95e4efc7a4362d80a707979e90c2a9f
jmasalunga2025-arch <jmasalunga2025@student.nbscollege.edu.ph>
2026-02-03 20:11:03 +0900

    Delete Activity1.py

--- Activity1.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-age += 1
-
-print(f"Hello, {name}. You will be {age} in one year.")
-print(f"Your name has {len(name)} characters.")
